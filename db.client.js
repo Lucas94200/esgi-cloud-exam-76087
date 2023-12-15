c8e26f14d14c69939621828a75a519eb4e780034
Lucas Yao <114393993+Lucas94200@users.noreply.github.com>
2023-12-15 10:49:10 +0100

    Update db.client.js

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // TODO
+  'postgres://db_esgi_cloud_exam_76087_user:Z5gXW0FkqrPDx3E7P28VDVzLCzuMfRyD@dpg-clu1ln21hbls73e8c1t0-a.frankfurt-postgres.render.com/db_esgi_cloud_exam_76087', // TODO
   {
     dialectOptions: {
       ssl: {
